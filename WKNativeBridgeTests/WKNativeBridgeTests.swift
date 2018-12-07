e6f29007b42a2ac54cb9650d364d28f6b6a6eb3a
Mateusz Stawecki <mateusz.stawecki@othermedia.com>
2018-12-07 14:54:23 +0000

    Initial Commit

---
+++ WKNativeBridgeTests/WKNativeBridgeTests.swift
@@ -0,0 +1,34 @@
+//
+//  WKNativeBridgeTests.swift
+//  WKNativeBridgeTests
+//
+//  Created by Mateusz Stawecki on 07/12/2018.
+//  Copyright © 2018 Tapjet. All rights reserved.
+//
+
+import XCTest
+@testable import WKNativeBridge
+
+class WKNativeBridgeTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
